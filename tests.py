944a7cb499ecaeedffc275c0412d9899b12f93d1
Michael Dockter <michael@dockter.com>
2018-10-22 18:43:33 -0400

    issue-3 Working test cases

--- tests.py
+++ tests.py
@@ -7,23 +7,24 @@ import unittest
 
 from migrate import transform_add_list_unique_elements
 
+
 class Test_01(unittest.TestCase):
 
     def setUp(self):
-        self.test_directory = "tests/test-01/data"
+        self.test_directory = "tests/test-01"
 
-        with open(self.test_directory + "/original.json") as original_file:
+        with open(self.test_directory + "/data/original.json") as original_file:
             self.original_dictionary = json.load(original_file)
-        with open(self.test_directory + "/template.json") as template_file:
+        with open(self.test_directory + "/data/template.json") as template_file:
             self.template_dictionary = json.load(template_file)
-        with open(self.test_directory + "/final.json") as final_file:
+        with open(self.test_directory + "/data/final.json") as final_file:
             self.final_dictionary = json.load(final_file)
 
     def test_transform_add_list_unique_elements_01(self):
 
         # Create output directory.
 
-        output_directory = "tests/test-01/results"
+        output_directory = "{0}/results".format(self.test_directory)
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
@@ -33,13 +34,13 @@ class Test_01(unittest.TestCase):
 
         # Output result_dictionary.
 
-        output_filename = "{0}/result-{1}.json".format(output_directory, int(time.time()))
+        output_filename = "{0}/test-transform-add-list-unique-elements-01-{1}.json".format(output_directory, int(time.time()))
         with open(output_filename, "w") as output_file:
             json.dump(result_dictionary, output_file, sort_keys=True, indent=4)
 
         # Check results.
 
-        self.assertDictEqual(result_dictionary, self.final_dictionary, "MJD message")
+        self.assertDictEqual(result_dictionary, self.final_dictionary, "Dictionaries are not equal")
 
 
 if __name__ == '__main__':
